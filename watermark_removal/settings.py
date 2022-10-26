68f5028bf4cd589589570fc01d11302f9d1d9509
Alex-Jee <jxfmail@foxmail.com>
2022-10-26 10:52:00 +0800

    .

--- watermark_removal/settings.py
+++ watermark_removal/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-gcy+ekzukshpi7&4n9o-uzh%5fiv6n(j0()9wy8mpc9ej%)l6i"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
